afb2d34afd8a60c4b9488addc2810913b854c036
shahrutul1993 <shahrutul111@gmail.com>
2020-10-22 19:01:16 +0530

    created avatars for users

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,10 @@
+module ApplicationHelper
+
+	# def user_avatar(user, size=40)
+ #    if user.avatar.attached?
+ #      user.avatar.variant(resize: "#{size}x#{size}!")
+ #    end
+ #  end
+
+  
+end
